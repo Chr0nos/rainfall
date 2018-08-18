68e4505a9bb2a0445dcb9e714530d6b25f4ec795
Sebastien NICOLET <snicolet@student.42.fr>
2018-08-18 22:02:53 +0200

    level2

--- level2/source.c
+++ level2/source.c
@@ -2,10 +2,19 @@
 
 int    p(void)
 {
-    char    buf[80];
-    char    x[104 - 80];
+    char            buf[64];
+    unsigned int    *addr;          // esp
 
-
+    fflush(stdout);
+    gets(buf);
+    addr = &buf[80];
+    if ((addr & (unsigned int *)0xb0000000) == 0xb0000000)
+    {
+        printf("(%p)\n", addr);
+        exit(0);
+    }
+    puts(buf);
+    return ((int)ft_strdup(buf));
 }
 
 int     main(void)
