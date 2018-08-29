d13f0deec0891f36fb09d535fb40fc1c41c60761
Sebastien NICOLET <snicolet@student.42.fr>
2018-08-29 18:48:59 +0200

    wip level9

--- level9/source.c
+++ level9/source.c
@@ -1,10 +1,24 @@
+#include <string.h>
+#include <unistd.h>
+
+// le code original semble etre en C++, donc ce code ne sera pas aussi
+// exaustif que les autres.
+
+struct      truc {
+    char    buf[100];
+    int     value;
+    int     useless;
+};
+
 int         main(int ac, char **av)
 {
-    char    buf[20];
+    struct truc        alpha;
+    struct truc        bravo;
 
     if (ac < 2)
         _exit(1);
-    char    x[108];
-    _Znwj()
-    return (0);
+    alpha.value = 5;
+    bravo.value = 6;
+    memcpy(alpha.buf, av[1], strlen(av[1]));
+    return (alpha.value + bravo.value);
 }
